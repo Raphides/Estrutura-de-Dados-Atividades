aa1592e73acaedf36cf9c6091d499e7e43184731
Raphael Mendes <raphaelmendesdasilva@gmail.com>
2022-10-30 20:10:36 -0300

    Correção do Exercício 6

--- Lista 1/Exercicio6.c
+++ Lista 1/Exercicio6.c
@@ -6,13 +6,15 @@ int main(int argc, char const *argv[])
     int lines;
     int counter = 1;
     int Joao, Ze;
+    int difference = 0;
     for (scanf("%d", &lines); lines != 0; scanf("%d", &lines))
     {
         printf("Teste %d\n", counter);
         for (int i = 0; i < lines; i++)
         {
             scanf("%d %d", &Joao, &Ze);
-            printf("%d\n", Joao - Ze);
+            difference += Joao - Ze;
+            printf("%d\n", difference);
         }
         printf("\n");
         counter++;

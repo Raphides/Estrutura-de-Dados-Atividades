6cb9774fe6b80492faf38da9ef76e4e4b9e5786c
Raphides <raphaelmendesdasilva@gmail.com>
2022-12-03 23:26:13 -0300

    Atualização movex

--- Lista_3/movex.c
+++ Lista_3/movex.c
@@ -4,7 +4,9 @@
 
 char *message;
 char *allXs;
-char input;
+char chr;
+char *input;
+int counter = 0;
 int counter_x = 0;
 int counter_message = 0;
 
@@ -15,24 +17,29 @@ int main(int argc, char const *argv[])
 {
     message = (char *) calloc(100, sizeof(char));
     allXs = (char *) calloc(100, sizeof(char));
+    input = (char *) calloc(100, sizeof(char));
+    scanf("%[^\n]s", input);
     printf("%s\n", movex());
     return 0;
 }
 
 char *movex(void){
-    if (scanf("%c", &input) != EOF && input != '\n')
+    chr = input[counter];
+    if (chr != '\0')
     {
-        if (input == 'x')
+        if (chr == 'x')
         {
-            allXs[counter_x] = input;
+            allXs[counter_x] = chr;
             counter_x++;
         }
         else {
-            message[counter_message] = input;
+            message[counter_message] = chr;
             counter_message++;
         }
+        counter++;
         movex();  
     } else {
+        free(input);
         message = (char *) realloc(message, sizeof(char) * counter_message);
         allXs = (char *) realloc(allXs, sizeof(char) * counter_x);
         return strcat(message, allXs);

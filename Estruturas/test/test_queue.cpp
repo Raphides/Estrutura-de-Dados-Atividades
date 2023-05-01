7472fedb6686a4c2863f0cf804b4e9442b642cab
Raphides <raphaelmendesdasilva@gmail.com>
2023-05-01 10:20:54 -0300

    Rebase e teste

--- Estruturas/test/test_queue.cpp
+++ Estruturas/test/test_queue.cpp
@@ -2,9 +2,10 @@
 # include <cstdio>
 # include <gtest/gtest.h>
 # include "../C/queue.h"
-// # include "queue.c"
+# include "../C/queue.c"
 
-int main(int argc, char const *argv[])
+namespace tests {
+TEST(queueElementsTest, ultimo)
 {
     linked_data_t * fila = (linked_data_t*) malloc(sizeof(linked_data_t));
     initQueue(fila);
@@ -15,11 +16,16 @@ int main(int argc, char const *argv[])
     queueElement(fila, 77);
     unqueueElement(fila);
     element_t* num = (element_t*) malloc(sizeof(element_t));
-    printf("Resultado: %d\n", getElement(fila, 3, num));
-    printf("Tamanho: %d\nIndex: %d\nElemento: %d\n", fila->length, 3, *num);
-    free(num);
+    getElement(fila, 3, num);
     deallocNodes(fila);
     free(fila);
-    printf("Success\n");
-    return RUN_ALL_TESTS();
+    ASSERT_EQ(num, 66);
+    free(num);   
+}
+}
+
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }

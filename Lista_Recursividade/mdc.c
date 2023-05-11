0ac0909d2aa5f479bc0b47360baa383eaa6d4e02
Raphides <89037051+Raphides@users.noreply.github.com>
2023-05-11 17:12:38 -0300

    Mmc e mdc

--- Lista_Recursividade/mdc.c
+++ Lista_Recursividade/mdc.c
@@ -1,41 +1,54 @@
 # include <stdio.h>
 # include <stdlib.h>
-# include <math.h>
 
-int mdc_aux(int a, int b)
+int mdc(int numero1, int numero2);
+
+int mmc(int numero1, int numero2);
+
+int mdc_aux(int maior, int menor)
 {
-    int resto = a % b;
+    int resto = maior % menor;
     if (resto == 0)
-        return abs(b);
+        return menor;
     else
-        return mdc_aux(b, resto);
+        return mdc_aux(menor, resto);
+}
+
+int mdc(int numero1, int numero2)
+{
+    numero1 = abs(numero1);
+    numero2 = abs(numero2);
+    if (numero1 == 0)
+    {
+        if (numero1 == 0 && numero2 == 0)
+        {
+            return -1;
+        }
+        return numero2;
+    }
+    else if (numero2 == 0)
+        return numero1;
+    else if (numero1 >= numero2)
+        return mdc_aux(numero1, numero2);
+    else
+        return mdc_aux(numero2, numero1);
     
 }
 
-int mdc(int a, int b)
+int mmc(int numero1, int numero2)
 {
-    if (a == 0)
-    {
-        if (a == 0 && b == 0)
-            return -1;
-        else
-            return b;
-    }
-    else if (b == 0)
-        return a;
+    if (numero1 < 0 || numero2 < 0)
+        return -1;
     else
     {
-        if (abs(a) < abs(b))
-            return mdc_aux(b, a);
-        else
-            return mdc_aux(a, b);  
+        return ((numero1 * numero2) / mdc(numero1, numero2));  
     }
 }
 
+
+
 int main(int argc, char const *argv[])
 {
-    int m, n;
-    scanf("%d %d", &m, &n);
-    printf("Resultado: %d\n", mdc(m,n));
+    printf("%d\n", mmc(12, 60));
     return 0;
 }

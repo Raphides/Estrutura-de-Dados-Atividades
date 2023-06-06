4f08659e04e961320abb8d1ff141ee9c0a147ebe
Raphides <89037051+Raphides@users.noreply.github.com>
2023-06-06 11:51:32 -0300

    Conserto do csv_reader.c

--- Listas/Nilton/Lista_Busca/csv_reader.c
+++ Listas/Nilton/Lista_Busca/csv_reader.c
@@ -31,15 +31,19 @@ int __count_columns(CSV* csv)
 int __count_lines(CSV* csv)
 {
     FILE* csvSRC = fopen(csv->path, "r");
-    char line[100];
     size_t count = -1;
-    while (fgets(line, 100, csvSRC) != NULL)
-    {        
-        count += 1;
+    char chr;
+    while (1)
+    {       
+        chr = fgetc(csvSRC);
+        if (chr == EOF)
+            break;
+        else if (chr == '\n')
+            count += 1;
     }
     if (count >= 0)
     {
-        csv->lines = count;
+        csv->lines = count + 1;
         fclose(csvSRC);
         return 1;
     }
@@ -51,23 +55,35 @@ int __count_lines(CSV* csv)
 void __get_pkey(CSV* csv, int pkey_column_index)
 {
     FILE* csvSRC = fopen(csv->path, "r");
-    int column = 0, line = 0;
+    int column = 0, line = 0, char_counter = 0;
     char chr, lines[100];
     fgets(lines, 99, csvSRC);
+    csv->pkeys[0] = (char*) malloc(100);
     do
     {
         chr = fgetc(csvSRC);
         if (csv->separator == chr)
+        {
             column++;
+        }
         else if (chr == '\n')
         {
+            csv->pkeys[line] = (char*) realloc(csv->pkeys[line], char_counter + 1);
             line++;
+            csv->pkeys[line] = (char*) malloc(100);
             column = 0;
+            char_counter = 0;
         }
         else if (chr == EOF)
+        {
+            csv->pkeys[line] = (char*) realloc(csv->pkeys[line], char_counter + 1);
             break;
+        }
         else if (column == pkey_column_index)
-            csv->pkeys[line] = csvSRC;
+        {
+            csv->pkeys[line][char_counter] = chr;
+            char_counter++;
+        }
     } while (1);
     fclose(csvSRC);
 }
@@ -86,7 +102,7 @@ CSV* to_csv(char* csv_path, char separator, int pkey)
         if (__count_columns(csv_file) && __count_lines(csv_file))
         {
             csv_file->head = (char**) malloc((csv_file->columns+1) * sizeof(char*));
-            csv_file->pkeys = (FILE**) malloc((csv_file->lines+1) * sizeof(FILE*));
+            csv_file->pkeys = (char**) malloc((csv_file->lines+1) * sizeof(char*));
             __get_pkey(csv_file, pkey);
             return csv_file;
         }
@@ -98,11 +114,31 @@ CSV* to_csv(char* csv_path, char separator, int pkey)
     }
 }
 
+int __pkey_aux_search(CSV* csv,char* key)
+{  
+    
+}
+
+int csv_pkey_search(CSV* csv, char* key)
+{
+    if (key == NULL)
+        return 0;
+    else
+        return __pkey_aux_search(csv,key);
+}
+
+
+
+
 int unnassing_csv(CSV* csv)
 {
     if (csv == NULL)
         return 0;
     free(csv->head);
+    for (int i = 0; i < csv->lines; i++)
+    {
+        free(csv->pkeys[i]);
+    }
     free(csv->pkeys);
     free(csv);
     return 1;
